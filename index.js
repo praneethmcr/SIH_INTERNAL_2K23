e93ba9370a02cd2092ce7d57fdafd3f9aeb5501d
mrchinnu <ganesh_mcr@srekrec.edu.in>
2023-09-17 14:37:42 +0530

    gan

--- index.js
+++ index.js
@@ -76,57 +76,7 @@ const teams = new mongoose.model("teams",TeamSchema)
 const jury = new mongoose.model("juries",JurySchema)
 const peerevals = new mongoose.model("peerevals",PeerevaluationSchema)
 
-// Create an HTTP server
-const server = http.createServer(app);
 
-// Create a WebSocket server by passing the HTTP server
-const wss = new WebSocket.Server({ server });
-
-// Initialize timer variables
-let timerValue = 24 * 60 * 60; // 24 hours in seconds
-let timerInterval;
-
-// Function to start the timer
-function startTimer() {
-    if (!timerInterval) {
-        timerInterval = setInterval(() => {
-            if (timerValue > 0) {
-                timerValue--;
-            }
-            broadcastTimer();
-        }, 1000);
-    }
-}
-
-// Function to broadcast timer updates to all connected clients
-function broadcastTimer() {
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ timerValue }));
-        }
-    });
-}
-
-
-
-// WebSocket connection handling
-wss.on('connection', (ws) => {
-    // Send the current timer value to the newly connected client
-    ws.send(JSON.stringify({ timerValue }));
-
-    // Listen for messages from the client (e.g., start timer)
-    ws.on('message', (message) => {
-        const data = JSON.parse(message);
-        if (data.action === 'startTimer') {
-            startTimer();
-        }
-    });
-});
-
-const PORT = process.env.PORT || 3838;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 app.get("/",(req,res)=>{
 res.render("index.ejs");
